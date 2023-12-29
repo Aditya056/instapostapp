569322d9b95a05b861447f4eb7a46a4dff1ae8c0
Aditya Koganti <adityakoganti18@gmail.com>
2023-12-29 17:25:22 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ const { IgApiClient } = require('instagram-private-api');
 const { get } = require('request-promise');
 const uploadImages = require("./ImageUpload");
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
   
   
 app.use(cors({
