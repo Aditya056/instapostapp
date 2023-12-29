8743704e62e367ff93d06f5948c9e24d09909705
Aditya Koganti <adityakoganti18@gmail.com>
2023-12-29 17:27:11 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -12,16 +12,11 @@ const uploadImages = require("./ImageUpload");
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-});
-  
-  
-app.use(cors({
-    origin: '*',
-}));
+  });
 
 app.use(express.json());
 
